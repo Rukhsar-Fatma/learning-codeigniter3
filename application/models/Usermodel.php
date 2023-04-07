eaead7a4a85688ed5cebb5624102289ceb570e08
Rukhsar Fatma <rukhsarfatma.shaikh@slashrtc.com>
2023-04-07 11:00:31 +0530

    built-in query and library

--- application/models/Usermodel.php
+++ application/models/Usermodel.php
@@ -3,10 +3,15 @@
 class Usermodel extends CI_model
 {
     public function getUserdata()
-    {
-        return [
-            ['Firstname'=>'Rukhsar','AccountNo'=>'89876787'],
-            ['Firstname'=>'Rukaiya','AccountNo'=>'89876333'],
-        ];
+    {   $this->load->library(array('form_validation'),('email'));
+        $this->form_validation->fb();
+        $this->email
+        $this->load->database();
+        // $this->db->select("firstname");
+        // $q=$this->db->get("users");
+        $q =$this->db->select('firstname,account_no')
+           ->where('id',1)
+           ->get('users');
+        return $q->result_array();
     }
 }
